306826e884350238a838e00064ce4acdde298044
power <samuelbkoroma7@gmail.com>
2022-04-08 14:46:37 +0000

    negative or positive

---
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+   * main - checks if positive, zero or negative
+   *
+    [D*Return: zero
+    [D*/
+int main(void)
+{
+        int n;
+        
+        srand(time()[D0)[C[D);
+        n = rand() -RAND_MAX / 2;
+        if (n > 0)
+         {
+                 printf("%d is positive\n", n);
+          }else if (n == 0)
+          {
+                  printf()[D"%d is zero\n", n);
+           }else
+           {
+                   printf(%d is negative\n"[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D"%d  is negative\n", n);
+           }
+            return(0)
+}
